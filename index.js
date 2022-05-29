97ba674b21b5cf981f67806160bb9b31979b9353
Napoleon Olaoluwa <anigboronapoleonolaoluwa@gmail.com>
2022-05-29 15:28:17 +0100

    Kasper work project

---
+++ index.js
@@ -0,0 +1,23 @@
+// ========== function for the payment popup =========
+const paymentPopup = document.getElementById('payment-popup');
+
+const openPaymentPopup = () => {
+    paymentPopup.classList.add('open-payment-popup');
+}
+
+const closePaymentPopup = () => {
+    paymentPopup.classList.remove('open-payment-popup');
+}
+
+// ========== function for the successsful popup =========
+
+const confirmPopup = document.getElementById('fail-popup');
+
+const opensuccPopup = () => {
+    confirmPopup.classList.add('open-fail-popup');
+    paymentPopup.classList.remove('open-payment-popup');
+}
+
+confirmPopup.addEventListener('click', () => {
+    confirmPopup.classList.remove('open-fail-popup');
+})
